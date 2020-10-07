97fc53bb8de4057ce80a797b1b49a9f50ffd99fc
Snehal Kadwe <snehalkadwe@gmail.com>
2020-10-07 22:58:58 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 86) {
+        $("#banner").addClass("shrink");
+    } else {
+        $("#banner").removeClass("shrink");
+    }
+});
